a3fd8ee7885b320087d00027ebf53ecaf116d874
Rajdeep Basak <90843663+rrbasak@users.noreply.github.com>
2022-03-04 19:46:16 +0530

    food website

---
+++ responsive.js
@@ -0,0 +1,8 @@
+
+var type=new Typed(".text",{
+    strings:["Welcome to MyOnlineMeal","Here you can find amazing foods","Get 25% discount ","Safe delivery","order now"],
+    typeSpeed:20,
+    backSpeed:20,
+    sleep:2,
+    loop:true,
+})
